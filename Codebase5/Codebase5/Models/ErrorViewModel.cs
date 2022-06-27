abce2ebdb061fda52221f6ba886d8d9ffeceb0a2
Tejasree Bayanagari <t.bayanagari@zensar.com>
2022-06-27 17:44:53 +0530

    codebase5

---
+++ Codebase5/Codebase5/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Codebase5.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
