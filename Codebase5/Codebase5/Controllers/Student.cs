829c96153c15d8466b375899fca2b0b37bd469af
Tejasree Bayanagari <t.bayanagari@zensar.com>
2022-07-11 12:46:33 +0530

    azure

---
+++ Codebase5/Codebase5/Controllers/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Codebase5.Controllers
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Course{ get; set; }
+         
+    }
+}
---
+++ Codebase5/Codebase5/Data/Codebase5Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Codebase5.Controllers;
+
+namespace Codebase5.Data
+{
+    public class Codebase5Context : DbContext
+    {
+        public Codebase5Context (DbContextOptions<Codebase5Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Codebase5.Controllers.Student>? Student { get; set; }
+    }
+}
